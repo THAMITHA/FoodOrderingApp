ea43b283adfd7179a7e7fdd265814f5729e741f8
Thamitha <thamitharajput@gmail.com>
2023-04-30 15:04:39 +0530

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello world from javascript!";
-const root = document.getElementById("root");
-root.appendChild(heading);
+const heading = React.createElement("h1",
+{id:"heading"},
+"Hello world from React!");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);

11cba859aff7af5fcb4032360f391e7677040820
Thamitha <thamitharajput@gmail.com>
2023-04-30 18:27:37 +0530

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-const parent=React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},
-React.createElement("h1",{},"inside h1 heading")));
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child1"},
+React.createElement("h1",{},"inside h1 tag")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);

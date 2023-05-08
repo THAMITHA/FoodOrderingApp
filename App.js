bb7c44d2db32ae41a9ee31ed0f967668a6ef5f13
Thamitha <thamitharajput@gmail.com>
2023-05-09 04:20:28 +0530

    create react element using React.createElement

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";  
 
+//React.createElement =>object => HTMLElement(render)
+
+const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Namaste React🚀"
+    );
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);

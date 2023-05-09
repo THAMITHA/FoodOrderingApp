59db5f3ee7ec50ef82cb0f053778a2571b781b6f
Thamitha <thamitharajput@gmail.com>
2023-05-09 09:41:29 +0530

    Ep.03 part.03

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";  
+import ReactDOM from "react-dom/client";
 
 //React.createElement => ReactElement-JS Object => HTMLElement(render)
 
-const heading = React.createElement(
-    "h1",
-    {id: "heading"},
-    "Namaste React🚀"
-    );
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React🚀");
 
-console.log(heading);    
+console.log(heading);
 
 //JSX(transpiled before it receive the JS) -  PARCEL - Babel
 
-//JSX=> React.createElement=>ReactElement-JS Object =>HTMLElement(render)
-const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>
+//JSX=> Babel transpiles it to React.createElement=>ReactElement-JS Object =>HTMLElement(render)
+const jsxHeading = (<h1 className="head">Namaste React using JSX 🚀</h1>);
 
 console.log(jsxHeading);
 

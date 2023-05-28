da4b19135affe60350b6acaa02514ad855a4d0e3
Thamitha <thamitharajput@gmail.com>
2023-05-28 18:20:24 +0530

    add shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState,useEffect } from "react"; 
-
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   //local State Variable - super powerful variable
@@ -23,7 +23,7 @@ const Body = () => {
   }
 
   if(listOfRestaurants.length ===0){
-    return <h1>Loading...</h1>
+    return <Shimmer/>;
   }
   
 

b5ec2d330e93b46594de16cd63df8e9eb3970061
Thamitha <thamitharajput@gmail.com>
2023-05-29 05:15:15 +0530

    add search functionality using useState Hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,74 +1,63 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState,useEffect } from "react"; 
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
   //local State Variable - super powerful variable
 
-  const [listOfRestaurants,setListOfRestaurant  ] = useState([]);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
-  useEffect(()=>{
+  const [filterRestaurant, setFilterResturant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendered");
+
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
-  const fetchData= async()=>{
+  const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9717208&lng=77.6006245&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
 
-    //Optional Chaining 
+    //Optional Chaining
     setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-  }
+    setFilterResturant(json?.data?.cards[2]?.data?.data?.cards);
+  };
 
-  if(listOfRestaurants.length ===0){
-    return <Shimmer/>;
-  }
-  
-
-
-  //Normal JS Variable 
-  let listOfRestaurantsJS = [
-    {
-      data: {
-        id: "3369",
-        name: "Truffles",
-        cloudinaryImageId: "cd832b6167eb9f88aeb1ccdebf38d942",
-        cuisines: ["American", "Desserts", "Continental", "Italian"],
-        costForTwo: 45000,
-        deliveryTime: 31,
-        avgRating: "4.4",
-      },
-    },
-    {
-      data: {
-        id: "3370",
-        name: "KFC",
-        cloudinaryImageId: "cd832b6167eb9f88aeb1ccdebf38d942",
-        cuisines: ["American", "Desserts", "Continental", "Italian"],
-        costForTwo: 45000,
-        deliveryTime: 31,
-        avgRating: "3.6",
-      },
-    },
-    {
-      data: {
-        id: "3370",
-        name: "PizaHut",
-        cloudinaryImageId: "cd832b6167eb9f88aeb1ccdebf38d942",
-        cuisines: ["American", "Desserts", "Continental", "Italian"],
-        costForTwo: 45000,
-        deliveryTime: 31,
-        avgRating: "4.6",
-      },
-    },
-  ];
-
-
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <div className="filer">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //filter the restaurant cards and update the UI
+              //searchText
+              console.log(searchText);
+
+              const filterRestaurant = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilterResturant(filterRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filer-btn"
           onClick={() => {
@@ -82,7 +71,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant, index) => (
+        {filterRestaurant.map((restaurant, index) => (
           <RestaurantCard key={index} resData={restaurant} />
         ))}
       </div>
